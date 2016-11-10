49a4db9494440176b60929557ce806135085b541
Romper <na2206@naver.com>
2016-11-11 02:44:15 +0900

    GitTest 커밋 테스트 02

--- app/src/main/java/com/example/jaewon/gittest/MainActivity.java
+++ app/src/main/java/com/example/jaewon/gittest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.jaewon.gittest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1 (View view){
+
+        Toast.makeText(getApplicationContext(), "버튼이 눌림", Toast.LENGTH_LONG).show();
     }
 }
